f84288b41300082a6b1f5e79837878a0707cb86c
Keith Funk <Keith.C.Funk@gmail.com>
2021-07-21 21:26:52 -0600

    objection Model class pulled from objection, Listing class extended. index, show, create functions created for /listings

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 9000;
-const cors = require('cors')
+const cors = require('cors');
 
-// const knex = require('knex');
-// const configuration = knex('./knexfile').development
-// const database = knex(configuration)
 const database = require('./db');
-console.log(database)
 
-app.use(cors())
+const { Model } = require('objection');
+const { request } = require('express');
+Model.knex(database);
 
-app.get('/listings', (_, response) => {
-    database('listing')
+app.use(cors());
+app.use(express.json());
+
+class Listing extends Model {
+    static tableName = 'listing'
+};
+
+app.get('/listings', index);
+app.get('/listings/:id', show)
+app.post('/listings', create)
+
+function index(_,response){
+    Listing.query()
         .then(listings => response.status(200).send(listings))
-        
-})
+};
 
-app.listen(port, () => console.log(`LISTENING ON PORT ${port}`))
+function show(request, response){
+    Listing.query()
+        .where('id', request.params.id)
+        .then(listings => response.status(200).send(listings[0]))
+};
+
+function create(request, response){
+    const { listing } = request.body
+    Listing.query()
+        .insert(listing)
+        .returning('*')
+        .then(listing => response.status(201).send(listing))
+}
+
+
+app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
