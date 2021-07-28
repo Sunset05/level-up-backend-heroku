cb4bb4b6e224806fade10a921673649672f1c7cb
Keith Funk <Keith.C.Funk@gmail.com>
2021-07-28 17:30:28 -0600

    added resource to allow a user to post new listings

--- routes/listings.js
+++ routes/listings.js
@@ -11,9 +11,9 @@ router.get('/listings', authenticate, (request, response) => {
 
 router.post('/listings', authenticate, (request, response) => {
     const { listing } = request.body;
-
+    const { user } = request
     Listing.query()
-        .insert({...listing, userId: request.user.id})
+        .insert({...listing, userId: user.id})
         .then(newListing => response.status(201).send(newListing))
 });
 
