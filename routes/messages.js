dd19c14706f5f560c741c25eaa9f109f2194259c
Keith Funk <Keith.C.Funk@gmail.com>
2021-08-03 22:08:37 -0600

    post route for messages created

--- routes/messages.js
+++ routes/messages.js
@@ -3,18 +3,13 @@ const router = express.Router();
 const { authenticate } = require('./auth') 
 const { Message } = require('../models/message');
 
+
 router.post('/messages', authenticate, (request, response) => {
     const { message } = request.body
-    console.log(message)
     Message.query()
         .insert({...message, sender: request.user.id})
         .returning('*')
         .then(createdMessage => response.status(201).send(createdMessage))
 });
-// const { listing } = request.body;
-// const { user } = request
-// Listing.query()
-//     .insert({...listing, userId: user.id})
-//     .then(newListing => response.status(201).send(newListing))
 
 module.exports = { messagesRouter:router };
