1613ddde3b92a287f918ef8adef658bc913cd8d1
Keith Funk <Keith.C.Funk@gmail.com>
2021-09-01 17:54:34 -0600

    configured production in knexfile

--- knexfile.js
+++ knexfile.js
@@ -8,4 +8,17 @@ module.exports = {
       password: "funk",
     },
   },
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: __dirname + "/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/seeds",
+    },
+  },
 };
