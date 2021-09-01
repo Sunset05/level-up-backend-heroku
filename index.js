2e42ca13221be71332725851b1691c5e38600784
Keith Funk <Keith.C.Funk@gmail.com>
2021-09-01 17:46:38 -0600

    added Procfile, modified port on index.js ro allow for development server, engine specified in package.json

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = 9000;
-const cors = require('cors');
-const { usersRouter } = require('./routes/users')
-const { listingsRouter } = require('./routes/listings')
-const { messagesRouter } = require('./routes/messages')
+const port = process.env.PORT || 9000;
+const cors = require("cors");
+const { usersRouter } = require("./routes/users");
+const { listingsRouter } = require("./routes/listings");
+const { messagesRouter } = require("./routes/messages");
 
 app.use(cors());
 app.use(express.json());
 app.use(usersRouter, listingsRouter, messagesRouter);
-
 
 app.listen(port, () => console.log(`LISTENING ON PORT ${port}`));
